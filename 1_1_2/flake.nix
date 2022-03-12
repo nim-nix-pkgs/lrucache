022d76d52fd8601744c19285d2d7142b8b202aab
hugosenari <hugosenari@gmail.com>
2022-03-12 03:07:54 -0300

    chore: re index lrucache

---
+++ 1_1_2/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Least recently used (LRU) cache'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-lrucache-1_1_2.flake = false;
+  inputs.src-lrucache-1_1_2.owner = "jackhftang";
+  inputs.src-lrucache-1_1_2.ref   = "refs/tags/1.1.2";
+  inputs.src-lrucache-1_1_2.repo  = "lrucache";
+  inputs.src-lrucache-1_1_2.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-lrucache-1_1_2"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-lrucache-1_1_2";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
